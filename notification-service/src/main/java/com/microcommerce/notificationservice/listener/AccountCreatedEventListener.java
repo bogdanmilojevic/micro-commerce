9be72b176e99ae447147965214a7ebdc2da77615
Bogdan Milojević <bogdan.milojevic@rbt.rs>
2023-11-28 14:38:47 +0100

    Move static topic configuration from listeners to application.yml file

--- notification-service/src/main/java/com/microcommerce/notificationservice/listener/AccountCreatedEventListener.java
+++ notification-service/src/main/java/com/microcommerce/notificationservice/listener/AccountCreatedEventListener.java
@@ -4,6 +4,7 @@ import com.microcommerce.notificationservice.data.event.AccountCreatedEvent;
 import com.microcommerce.notificationservice.service.EmailSenderService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.stereotype.Component;
@@ -15,8 +16,11 @@ public class AccountCreatedEventListener {
 
     private final EmailSenderService emailSenderService;
 
-    @KafkaListener(topics = {"account-topic"}, groupId = "group-id-2", containerFactory = "accountCreatedListenerContainerFactory")
+    @KafkaListener(topics = "${kafka.topics.account-topic}", groupId = "group-id-2", containerFactory = "accountCreatedListenerContainerFactory")
     public void listen(AccountCreatedEvent accountCreatedEvent) {
+
+        System.out.println("New account created " + accountCreatedEvent.getEmail());
+
         var message = new SimpleMailMessage();
         message.setTo(accountCreatedEvent.getEmail());
         message.setSubject("Micro-Commerce Account Confirmation");
--- notification-service/src/main/java/com/microcommerce/notificationservice/listener/OrderCreatedEventListener.java
+++ notification-service/src/main/java/com/microcommerce/notificationservice/listener/OrderCreatedEventListener.java
@@ -4,6 +4,7 @@ import com.microcommerce.notificationservice.data.event.OrderCreatedEvent;
 import com.microcommerce.notificationservice.service.OrderCreatedEventHandlerService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
@@ -17,7 +18,7 @@ public class OrderCreatedEventListener {
 
     private final OrderCreatedEventHandlerService orderCreatedEventHandler;
 
-    @KafkaListener(topics = {"notification-topic"}, groupId = "group-id", containerFactory = "orderCreatedEventListenerContainerFactory")
+    @KafkaListener(topics = "${kafka.topics.notification-topic}", groupId = "group-id", containerFactory = "orderCreatedEventListenerContainerFactory")
     public void listen(OrderCreatedEvent orderCreatedEvent) {
         log.info("Order created event received.");
         orderCreatedEventHandler.sendOrderConfirmedMail(orderCreatedEvent);
